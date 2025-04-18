e89c196c57256e2dccfc869b02d18a54e60fd3ce
Fabian Schambeck <fabian.schmbck@gmail.com>
2025-04-18 11:11:56 +0200

    updated project structure

--- IR_project/main.py
+++ IR_project/main.py
@@ -11,13 +11,13 @@ from src.dataset_utils import (
 
 def main():
     dataset = None
-    json_path = "data/harry_potter_dataset.json"
-    jsonl_path = "data/harry_potter_dataset.jsonl"
+    json_path = "data/processed/harry_potter_dataset.json"
+    jsonl_path = "data/processed/harry_potter_dataset.jsonl"
 
     # JSON
     if not os.path.exists(json_path):
         print("JSON not found — loading from .txt and saving to JSON.")
-        dataset = load_books_from_txt("data/")
+        dataset = load_books_from_txt("data/raw/")
         save_to_json(dataset, json_path)
     else:
         print("JSON already exists, skipping save.")
@@ -26,7 +26,7 @@ def main():
     if not os.path.exists(jsonl_path):
         if dataset is None:
             print("JSONL not found — loading from .txt and saving to JSONL.")
-            dataset = load_books_from_txt("data/")
+            dataset = load_books_from_txt("data/raw/")
         save_to_jsonl(dataset, jsonl_path)
     else:
         print("JSONL already exists, skipping save.")
---
+++ IR_project/src/preprocessing.py
@@ -0,0 +1,17 @@
+# hybrid retrieval: lexical and semantic
+# step 1: implement lexical search
+
+# NER?
+# Normalization: 3pm - 3h - 15:00
+# tokenization?
+# n-grams?
+
+# 1. Chunk long text
+# sentence-based (spacy)
+#
+# 2. Clean text
+#
+# 3. Add metadata
+#
+# 4. Store results
+
