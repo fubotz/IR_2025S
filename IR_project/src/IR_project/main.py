1277f631bd9239bb50f44ea358dfbf30e7276ba2
Fabian Schambeck <fabian.schmbck@gmail.com>
2025-05-09 18:37:13 +0200

    everything works

--- IR_project/src/IR_project/main.py
+++ IR_project/src/IR_project/main.py
@@ -13,23 +13,23 @@ def main():
     root_dir = Path(__file__).resolve().parents[3]      # IR_2025S/
 
     # define paths
-    raw_path = root_dir / "IR_project" / "data" / "raw"
-    processed_path = root_dir / "IR_project" / "data" / "processed"
+    raw_path = root_dir/"IR_project"/"data"/"raw"
+    processed_path = root_dir/"IR_project"/"data"/"processed"
 
-    json_path = processed_path / "dataset.json"
-    jsonl_path = processed_path / "dataset.jsonl"
+    json_path = processed_path/"dataset.json"
+    jsonl_path = processed_path/"dataset.jsonl"
 
-    # Load the chapters from raw .txt files
+    # load the chapters from raw .txt files
     dataset = list(load_books_from_txt(raw_path))
     print(f"✅ Loaded {len(dataset)} chapters from raw text.")
 
-    # Save as JSON and JSONL
+    # save as JSON and JSONL
     save_to_json(dataset, json_path)
     save_to_jsonl(dataset, jsonl_path)
     print(f"📁 Saved JSON to: {json_path}")
     print(f"📁 Saved JSONL to: {jsonl_path}")
 
-    # Optional: convert to HuggingFace Dataset
+    # convert to HuggingFace Dataset
     hf_dataset = convert_to_hf_dataset(dataset)
     print(f"🤗 HuggingFace Dataset created with {hf_dataset.num_rows} rows.")
     print(hf_dataset[0])
